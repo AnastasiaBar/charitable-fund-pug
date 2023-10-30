40626aa90999fb0f97ae3175f0fa66be8c9054f5
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-10-30 16:50:31 +0300

    feat: раздел hero, правки в burger

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,6 +1,26 @@
 import ready from "../../js/utils/documentReady.js";
+import Swiper, { Navigation, Pagination } from "swiper/bundle";
 
-ready(function() {
+Swiper.use([Navigation, Pagination]);
 
+ready(function () {
+  let galleryThumbs = new Swiper(".hero__slider-gallery", {
+    slidesPerView: 2,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+
+  new Swiper(".hero__main-slider", {
+    navigation: {
+      nextEl: ".hero__swiper-button-next",
+      prevEl: ".hero__swiper-button-prev",
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 });
-
