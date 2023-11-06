1ade3afbc492aa0337231164833a1b06852c41d4
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-11-07 00:42:11 +0300

    feat: раздел hero, раздел о компании

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -4,23 +4,97 @@ import Swiper, { Navigation, Pagination } from "swiper/bundle";
 Swiper.use([Navigation, Pagination]);
 
 ready(function () {
-  let galleryThumbs = new Swiper(".hero__slider-gallery", {
-    slidesPerView: 2,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
+  const colorSlider = ["#B3315E", "#dc9138", "#067256"];
+  let count = 0;
+  const elBottom = document.querySelector(".blob-bottom__icon-decor-bottom");
+  const elBottomHidden = document.querySelector(".hero__decor-bottom-disabled svg");
+  const containerElBottomHidden = document.querySelector(".hero__decor-bottom-disabled");
+  const elTopHidden = document.querySelector(".hero__decor-top-disabled svg");
+  const sliderBg = document.querySelector(".hero__wrapper");
+
+  Array.from(document.querySelectorAll(".slider"), (n) => {
+    return new Swiper(n, {
+      loop: true,
+      allowTouchMove: false,
+      speed: 1000,
+      navigation: {
+        nextEl: ".slider-arrows__swiper-button-next",
+        prevEl: ".slider-arrows__swiper-button-prev",
+      },
+      on: {
+        slideNextTransitionStart: function () {
+          sliderCounter(++count, false);
+        },
+        slidePrevTransitionStart: function () {
+          sliderCounter(--count, true);
+        },
+      },
+    });
   });
 
-  new Swiper(".hero__main-slider", {
-    navigation: {
-      nextEl: ".hero__swiper-button-next",
-      prevEl: ".hero__swiper-button-prev",
-    },
-    thumbs: {
-      swiper: galleryThumbs,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  function sliderCounter(countValue, slidePrev) {
+    count =
+      countValue === -1
+        ? colorSlider.length - 1
+        : countValue === colorSlider.length
+        ? 0
+        : countValue;
+    slidePrev === true ? changeSlidePrev() : changeSlideNext();
+  }
+
+  function changeSlidePrev() {
+    elTopHidden.classList.add("active");
+    sliderBg.style.background = colorSlider[count];
+    elBottom.classList.add("active-two");
+
+    setTimeout(() => {
+      elBottom.classList.remove("active-two");
+      styleElBottom();
+      Object.assign(elTopHidden.style, {
+        transition: "0s ease-out",
+        color: count === colorSlider.length - 1 ? colorSlider[count] : colorSlider[count - 1],
+      });
+    }, 500);
+
+    setTimeout(() => {
+      Object.assign(elTopHidden.style, {
+        transition: "",
+        color: colorSlider[count],
+      });
+      elTopHidden.classList.remove("active");
+      elBottom.style.transition = "1.2s ease-out";
+    }, 1000);
+  }
+
+  function changeSlideNext() {
+    elBottom.classList.add("active");
+    elBottom.style.transform = "scale(20)";
+    elTopHidden.style.display = "none";
+    containerElBottomHidden.classList.add("active");
+    elBottomHidden.style.color =
+      count === colorSlider.length - 1 ? colorSlider[0] : colorSlider[count + 1];
+
+    setTimeout(() => {
+      sliderBg.style.background = colorSlider[count];
+      elBottom.classList.remove("active");
+      styleElBottom();
+    }, 500);
+
+    setTimeout(() => {
+      elBottom.style = "";
+      elBottom.style.color =
+        count === colorSlider.length - 1 ? colorSlider[0] : colorSlider[count + 1];
+      containerElBottomHidden.classList.remove("active");
+      elTopHidden.style.display = "block";
+      elTopHidden.style.color = colorSlider[count];
+    }, 1000);
+  }
+
+  function styleElBottom() {
+    Object.assign(elBottom.style, {
+      transform: "scale(1)",
+      transition: "0s ease-out",
+      color: count === colorSlider.length - 1 ? colorSlider[0] : colorSlider[count + 1],
+    });
+  }
 });
